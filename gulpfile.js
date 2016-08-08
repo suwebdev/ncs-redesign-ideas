3f5b37aff1b5a1b4c14155b8b8c1542da002a848
Shawn Rider <shawn@shawnrider.com>
2016-08-08 15:27:40 -0700

    Added deployment task to Gulp.
    Change-Id: I541c46170e8b99513e8465ade1abde365132a20b

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,15 @@ const gulpLoadPlugins = require('gulp-load-plugins');
 const browserSync = require('browser-sync');
 const del = require('del');
 const wiredep = require('wiredep').stream;
+const ghPages = require('gulp-gh-pages');
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
 
 gulp.task('styles', () => {
   return gulp.src('app/styles/*.scss')
