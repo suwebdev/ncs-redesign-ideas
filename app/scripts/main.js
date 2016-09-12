cb8b1cf7158efb6f86951c7ffc4d013f4fc46de1
Shawn Rider <shawn@shawnrider.com>
2016-09-12 13:31:41 -0700

    Removed slide effects for the moment.
    Change-Id: Ibecd99fcebe91076185b0ddcdf5f22229ae62f0a

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,18 @@
 $(document).on('ready', function(){
     $(document).on('click', '.yamm .dropdown-menu', function(e) {
-      e.stopPropagation()
+      e.stopPropagation();
     });
 
-    // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
-    $('.dropdown').on('show.bs.dropdown', function(e){
-      $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
-      console.log('sliding down');
-    });
+    // // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
+    // $('.dropdown').on('show.bs.dropdown', function(e){
+    //   $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+    //   console.log('sliding down');
+    // });
 
-    // ADD SLIDEUP ANIMATION TO DROPDOWN //
-    $('.dropdown').on('hide.bs.dropdown', function(e){
-      $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
-      console.log('sliding up');
-    });
+    // // ADD SLIDEUP ANIMATION TO DROPDOWN //
+    // $('.dropdown').on('hide.bs.dropdown', function(e){
+    //   $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+    //   console.log('sliding up');
+    // });
 
 })
