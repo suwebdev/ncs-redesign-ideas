0872c2842dd9a04d639d30bc8611e5ee4b01477b
Shawn Rider <shawn@shawnrider.com>
2016-09-12 13:32:43 -0700

    Update 2016-09-12T20:32:41.031Z
    Change-Id: I4313a7e898044da590fb294c3e0c0be0288f7e7e

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-"use strict";$(document).on("ready",function(){$(document).on("click",".yamm .dropdown-menu",function(o){o.stopPropagation()}),$(".dropdown").on("show.bs.dropdown",function(o){$(this).find(".dropdown-menu").first().stop(!0,!0).slideDown(),console.log("sliding down")}),$(".dropdown").on("hide.bs.dropdown",function(o){$(this).find(".dropdown-menu").first().stop(!0,!0).slideUp(),console.log("sliding up")})});
+"use strict";$(document).on("ready",function(){$(document).on("click",".yamm .dropdown-menu",function(n){n.stopPropagation()})});
